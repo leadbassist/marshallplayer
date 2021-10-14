a6f91ff46547ee6d52de3e368318fe5c2d708145
leadbassist <leadbassist@live.com>
2021-10-14 03:06:50 -0400

    finished

---
+++ script.js
@@ -0,0 +1,134 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeElement = document.getElementById('current-time');
+const durationElement = document.getElementById('duration');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// music
+const songs = [
+    {
+        name: 'disciple',
+        displayName: 'Disciple',
+        artist: 'Slayer',
+    },
+    {
+        name: 'Pray For Me',
+        displayName: 'Pray For Me',
+        artist: 'Sixx AM',
+    },
+    {
+        name: 'Van Nuys',
+        displayName: 'Van Nuys',
+        artist: 'Sixx AM',
+    },
+    {
+        name: 'You Against You',
+        displayName: 'You Against You',
+        artist: 'Slayer',
+    },
+]
+
+// check if playing
+let isPlaying = false;
+
+// play
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+// pause
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+// play or pause event listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// current song
+let songIndex = 0;
+
+//on load - select first song
+loadSong(songs[songIndex]);
+
+// previous song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]); 
+    playSong();
+}
+
+// next song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// update progress bar and time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        // update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        // calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // delay switching duration element to avoid Nan:Nan
+        if (durationSeconds) {
+            durationElement.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // calculate display for current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeElement.textContent = `${currentMinutes}:${currentSeconds}`;
+
+    }
+}
+
+// set progress bar
+function setProgressBar (e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const { duration } = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
+// event listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
